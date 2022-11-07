ea8e0f7c2e3e9f6df621c830c7d7f04c3cac7384
Yulia Isakova <gunoffun@yandex.ru>
2022-11-07 20:20:55 +0300

    some updates

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import _ from 'lodash';
 import parser from './parsers.js';
 import formatter from '../formatters/index.js';
 
-const getFullPath = (filepath) => path.resolve(process.cwd(), '__fixtures__', filepath);
+const getFullPath = (filepath) => {
+  const arrFilepath = filepath.split('/');
+  filepath = arrFilepath[arrFilepath.length - 1];
+  return path.resolve(process.cwd(), '__fixtures__', filepath)
+}
 const buildTree = (obj1, obj2) => {
     const keys = [obj1, obj2].flatMap(Object.keys);
     const unionKeys = _.sortBy(_.union(keys));
@@ -17,8 +21,8 @@ const buildTree = (obj1, obj2) => {
           children: buildTree(value1, value2),
         };
       }
-      if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) { return { key, type: 'added', value: value2 }; }
-      if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) { return { key, type: 'removed', value: value1 }; }
+      if (!Object.hasOwn(obj1, key)) { return { key, type: 'added', value: value2 }; }
+      if (!Object.hasOwn(obj2, key)) { return { key, type: 'removed', value: value1 }; }
       if (value1 === value2) { return { key, type: 'unchanged', value: value1 }; }
       return {
         key, type: 'changed', value: value2, oldValue: value1,
@@ -28,10 +32,11 @@ const buildTree = (obj1, obj2) => {
   };
 
 const genDiff = (filepath1, filepath2, formatOffile) => {
-const format = filepath1.split('.')[1];
+const format1 = filepath1.split('.')[1];
+const format2 = filepath2.split('.')[1];
 const file1 = fs.readFileSync(getFullPath(filepath1), 'utf-8');
 const file2 = fs.readFileSync(getFullPath(filepath2), 'utf-8');
-const [parsingFile1, parsingFile2] = parser(file1, file2, format);
+const [parsingFile1, parsingFile2] = parser(file1, file2, format1, format2);
 const futer = buildTree(parsingFile1, parsingFile2);
 
 return formatter(futer, formatOffile);
--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,20 @@
 import yaml from 'js-yaml';
 
-const parser = (file1, file2, format) => {
-    if (format === 'json') {
-        return [JSON.parse(file1), JSON.parse(file2)];
+const parser = (file1, file2, format1, format2) => {
+    const result = [];
+    if (format1 === 'json') {
+        result.push(JSON.parse(file1));
     }
-    return [yaml.load(file1), yaml.load(file2)];
+    if (format1 === 'yaml' || format1 === 'yml') {
+        result.push(yaml.load(file1));
+    }
+    if (format2 === 'json') {
+        result.push(JSON.parse(file2));
+    }
+    if (format2 === 'yaml' || format2 === 'yml') {
+        result.push(yaml.load(file2));
+    }
+    return result;
 };
 
 export default parser;
